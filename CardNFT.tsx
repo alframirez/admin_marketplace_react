059abb2985c16a4498d89407119bec6e424144d7
alframirezg <alframirezg@gmail.com>
2021-11-14 13:05:32 -0400

    3 files modified

--- CardNFT.tsx
+++ CardNFT.tsx
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-
+{
+  //import logoDogma from 'assets/img/logoDogma.png';
+}
 class CardNFT extends Component {
   render() {
+    var priceNFT = 500;
     return (
       <div className="container">
         <div className="row">
-          <div className="col-4 text-center">
-            <h1>Funciona</h1>
+          <div className="col-lg-4 col-md-6 col-sm-12 text-center">
+            <div className="card mb-3">
+              <img
+                src="assets/img/logoDogma.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">NFT 1</h5>
+
+                <p className="card-text">
+                  <strong className="txt priceNFT"> {priceNFT} DMA </strong>
+                </p>
+                <button className="btn btn-dd">Comprar</button>
+              </div>
+            </div>
           </div>
-          <div className="col-4 text-center">
-            <h1>Funciona</h1>
+
+          <div className="col-lg-4 col-md-6 col-sm-12 text-center">
+            <div className="card mb-3">
+              <img
+                src="assets/img/logoDogma.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">NFT 1</h5>
+
+                <p className="card-text">
+                  <strong className="txt priceNFT"> {priceNFT} DMA </strong>
+                </p>
+                <button className="btn btn-dd">Comprar</button>
+              </div>
+            </div>
           </div>
-          <div className="col-4 text-center">
-            <h1>Funciona</h1>
+          <div className="col-lg-4 col-md-6 col-sm-12 text-center">
+            <div className="card mb-3">
+              <img
+                src="assets/img/logoDogma.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">NFT 1</h5>
+
+                <p className="card-text">
+                  <strong className="txt priceNFT"> {priceNFT} DMA </strong>
+                </p>
+                <button className="btn btn-dd">Comprar</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
